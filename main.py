048bbe504d702ac000598087ea5fffe01f2e9fa6
danilsizov1 <danilsizov3@gmail.com>
2016-11-26 06:55:01 +0200

    Some features

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import requests
 
 class HttpProcessor(BaseHTTPRequestHandler):
     def do_GET(self):
-        response = requests.get('http://localhost:80')
+        response = requests.get('https://whinny.herokuapp.com/')
         print(response.content)
         self.send_response(200)
         self.send_header('content-type','text/html')
         self.end_headers()
-        url = "http://uploads.ru/api?upload"
+        url = "https://whinny.herokuapp.com/"
         file = {"file": open("start.jpg", "rb")}
         my_session = requests.Session()
         log = my_session.post(url, files=file)
         print(log.text)
         self.wfile.write('hi')
 
-serv = HTTPServer(("localhost",80),HttpProcessor)
+serv = HTTPServer(("https://whinny.herokuapp.com/",80),HttpProcessor)
 serv.serve_forever()
